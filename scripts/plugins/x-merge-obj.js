8892c9f0e223b971b04d0424acda15fd8c27a4f0
ndiezel0 <ndiezel0@gmail.com>
2019-09-09 20:44:45 +0300

    CAPI-381: Fix build (#2)
    * CAPI-381: remove unused x-merge-properties from definitions
    
    * CAPI-381: revert x-merge-properties changes
    
    * Revert "CAPI-381: remove unused x-merge-properties from definitions"
    
    This reverts commit ae7ae9ba
    
    * CAPI-381: rename `x-merge-properties` to `x-merge-obj`

--- scripts/plugins/x-merge-obj.js
+++ scripts/plugins/x-merge-obj.js
@@ -2,28 +2,25 @@ var jpointer = require('json-pointer');
 var mergePatch = require('json-merge-patch');
 
 module.exports = {
-  pathExpression: '$..["x-merge-properties"]',
+  pathExpression: '$..["x-merge-obj"]',
   init: function(swagger) {
-    console.log('* x-merge-properties plugin');
+    console.log('* x-merge-obj plugin');
   },
   process: function(parent, name, jsonpath, swagger) {
     var value = parent[name];
     if (!Array.isArray(value)) {
-      throw Error('x-merge-properties argument should be array at ' + jsonpath);
+      throw Error('x-merge-obj argument should be array at ' + jsonpath);
     }
-    let required = [];
-    let properties = {};
+    let res = null;
     value.forEach(function(obj) {
+      if (typeof obj !== 'object') throw Error('Can\'t merge non-object values at ' + jsonpath);
       if (obj.$ref && (typeof obj.$ref === 'string')) {
         obj = jpointer.get(swagger, obj.$ref.substring(1));
       }
-      if (typeof obj !== 'object') throw Error('Can\'t merge non-object values at ' + jsonpath);
-      required = required.concat(obj.required || []);
-      properties = mergePatch.apply(properties, obj.properties || {});
+      res = mergePatch.apply(res, obj);
     });
     delete parent[name];
-    parent.required = required;
-    parent.properties = properties;
+    Object.assign(parent, res);
   },
   finish: function(swagger) {
     // TODO: cleanup unused $refs
